a0660ca9ce6eff5d98c8aae8a701ed53c7327cd1
Daniel Krastev <danielkrastev01@gmail.com>
2025-02-23 21:20:41 +0200

    Updated template

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,36 @@ import './App.css'
 
 function App() {
 
+  const addSymbolInField = (e) => {
+    console.log(e.target.innerText);
+    
+  }
+
+
   return (
     <>
-    <div class="container">
-      <form action="" name="calc" class="calculator">
-        <input type="text" class="value" readonly name="txt" />
-        <span class="num clear" onclick="calc.txt.value=''"><i>C</i></span>
-        <span class="num" onclick="calc.txt.value+='/'"><i>/</i></span>
-        <span class="num" onclick="calc.txt.value+='*'"><i>*</i></span>
-        <span class="num" onclick="calc.txt.value+='7'"><i>7</i></span>
-        <span class="num" onclick="calc.txt.value+='8'"><i>8</i></span>
-        <span class="num" onclick="calc.txt.value+='9'"><i>9</i></span>
-        <span class="num" onclick="calc.txt.value+='-'"><i>-</i></span>
-        <span class="num" onclick="calc.txt.value+='4'"><i>4</i></span>
-        <span class="num" onclick="calc.txt.value+='5'"><i>5</i></span>
-        <span class="num" onclick="calc.txt.value+='6'"><i>6</i></span>
-        <span class="num plus" onclick="calc.txt.value+='+'"><i>+</i></span>
-        <span class="num" onclick="calc.txt.value+='1'"><i>1</i></span>
-        <span class="num" onclick="calc.txt.value+='2'"><i>2</i></span>
-        <span class="num" onclick="calc.txt.value+='3'"><i>3</i></span>
-        <span class="num" onclick="calc.txt.value+='0'"><i>0</i></span>
-        <span class="num" onclick="calc.txt.value+='00'"><i>00</i></span>
-        <span class="num" onclick="calc.txt.value+='.'"><i>.</i></span>
+    <div className="container">
+      <form name="calc" className="calculator">
+        <input type="text" className="value" readOnly />
+        <span className="num clear"><i>C</i></span>
+        <span className="num" onClick={addSymbolInField}><i>/</i></span>
+        <span className="num" onClick={addSymbolInField}><i>*</i></span>
+        <span className="num" onClick={addSymbolInField}><i>7</i></span>
+        <span className="num" onClick={addSymbolInField}><i>8</i></span>
+        <span className="num" onClick={addSymbolInField}><i>9</i></span>
+        <span className="num" onClick={addSymbolInField}><i>-</i></span>
+        <span className="num" onClick={addSymbolInField}><i>4</i></span>
+        <span className="num" onClick={addSymbolInField}><i>5</i></span>
+        <span className="num" onClick={addSymbolInField}><i>6</i></span>
+        <span className="num plus" onClick={addSymbolInField}><i>+</i></span>
+        <span className="num" onClick={addSymbolInField}><i>1</i></span>
+        <span className="num" onClick={addSymbolInField}><i>2</i></span>
+        <span className="num" onClick={addSymbolInField}><i>3</i></span>
+        <span className="num" onClick={addSymbolInField}><i>0</i></span>
+        <span className="num" onClick={addSymbolInField}><i>00</i></span>
+        <span className="num" onClick={addSymbolInField}><i>.</i></span>
 
-        <span class="num equal" onclick="document.calc.txt.value=eval(calc.txt.value)"><i>=</i></span>
+        <span className="num equal"><i>=</i></span>
       </form>
   </div>
     </>
