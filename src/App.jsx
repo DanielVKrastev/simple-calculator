45d56d54d27603b1f4c820f8ad11c7d0326c3240
Daniel Krastev <danielkrastev01@gmail.com>
2025-02-23 22:28:56 +0200

    Added function for add symbol in calc field
    Without repeating characters

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,49 @@
+import { useEffect, useState } from 'react';
 import './App.css'
 
 function App() {
-
+  
   const addSymbolInField = (e) => {
-    console.log(e.target.innerText);
+    let symbol = e.target.innerText;
+    const regex = /^[0-9]/;
+    if(symbol.match(regex)){
+      symbol = Number(symbol);
+    }
+
+    // TODO: +/-
+
+    const calcField = document.getElementById('calc-field');
+    const textCalcField = calcField.value;
+    const previousSymbol = textCalcField[textCalcField.length - 1];
+
+    if(typeof(symbol) === 'number' && textCalcField[0] === '0'){
+      calcField.value = '';
+    }
+
+    if(typeof(symbol) === 'string'){
+      switch (previousSymbol) {
+        case '.':
+        case '*':
+        case '/':
+        case '-':
+        case '+':
+          return;
+      }
+    }
     
+    calcField.value += symbol;
+
+    
+  }
+
+  const clearField = (e) => {
+    document.getElementById('calc-field').value = '0';
+  }
+
+  const calculate = () => {
+    const calcField = document.getElementById('calc-field');
+    const textCalcField = calcField.value;
+
   }
 
 
@@ -12,8 +51,8 @@ function App() {
     <>
     <div className="container">
       <form name="calc" className="calculator">
-        <input type="text" className="value" readOnly />
-        <span className="num clear"><i>C</i></span>
+        <input type="text" className="value" readOnly id="calc-field" value="0"/>
+        <span className="num clear" onClick={clearField}><i>C</i></span>
         <span className="num" onClick={addSymbolInField}><i>/</i></span>
         <span className="num" onClick={addSymbolInField}><i>*</i></span>
         <span className="num" onClick={addSymbolInField}><i>7</i></span>
@@ -27,11 +66,11 @@ function App() {
         <span className="num" onClick={addSymbolInField}><i>1</i></span>
         <span className="num" onClick={addSymbolInField}><i>2</i></span>
         <span className="num" onClick={addSymbolInField}><i>3</i></span>
+        <span className="num" onClick={addSymbolInField}><i>+/-</i></span>
         <span className="num" onClick={addSymbolInField}><i>0</i></span>
-        <span className="num" onClick={addSymbolInField}><i>00</i></span>
         <span className="num" onClick={addSymbolInField}><i>.</i></span>
 
-        <span className="num equal"><i>=</i></span>
+        <span className="num equal" onClick={calculate}><i>=</i></span>
       </form>
   </div>
     </>
